c592f8c09bec83922e7ee2b98c38a046aa8f0ed7
Adrian Borup <adrian@borup.com>
2023-10-25 17:28:54 +0200

    Receive file path and width from command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
+use converter::ImageToBrailleConverter;
+
 mod braille;
 mod converter;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let converter = converter::ImageToBrailleConverter::from_file("goose.jpg")?.resize(60);
+    let args: Vec<String> = std::env::args().collect();
+
+    let file_path = match &args[..] {
+        [_, file_path, ..] => file_path,
+        _ => {
+            eprintln!("Usage: {} <file_path> [width]", args[0]);
+            std::process::exit(1);
+        }
+    };
+
+    let width = args
+        .get(2)
+        .and_then(|s| s.parse::<usize>().ok())
+        .unwrap_or(60);
+
+    let converter = ImageToBrailleConverter::from_file(file_path)?.resize(width);
 
     let mut stdout = std::io::stdout().lock();
     converter.convert(&mut stdout)?;
